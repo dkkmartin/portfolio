eae2d72e55eb205aa50b2395e5af7c0c87c3e0c1
Martin <martinbruun1995@gmail.com>
2023-11-01 19:01:51 +0100

    Added projects

--- src/scripts/projectSection.ts
+++ src/scripts/projectSection.ts
@@ -15,20 +15,3 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 })
-
-const cards = document.querySelectorAll<HTMLElement>('.card__video')
-
-cards.forEach((element) => {
-  const img = element.querySelector('img') as HTMLImageElement
-  const video = element.querySelector('video') as HTMLVideoElement
-  img.addEventListener('mouseenter', () => {
-    img.style.display = 'none'
-    video.style.display = 'block'
-    video.play()
-  })
-  video.addEventListener('mouseleave', () => {
-    img.style.display = 'block'
-    video.style.display = 'none'
-    video.pause()
-  })
-})
