b4bdab51c9693a4e28db30bc428ab02ad553d4b4
Martin Bruun <martinbruun1995@gmail.com>
2024-09-14 01:05:30 +0200

    feat: icon component

---
+++ src/components/Icon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	export let name: string;
+	export let size: number = 24;
+	export let className: string | undefined = undefined;
+	let svg: string;
+
+	$: import(`$lib/svg/${name}.svg`).then((module) => (svg = module.default));
+</script>
+
+{#if svg}
+	<img
+		loading="eager"
+		src={svg}
+		alt={name}
+		width={size}
+		height={size}
+		class={className ? cn(className) : ''}
+	/>
+{/if}
