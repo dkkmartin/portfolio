40dedca128503604c3f7ac518082c3520a3cd5db
Martin Bruun <martinbruun1995@gmail.com>
2024-09-14 01:05:22 +0200

    feat: hero text

---
+++ src/components/Hero.svelte
@@ -0,0 +1,7 @@
+<script>
+	import WordsFadeIn from './words/WordsFadeIn.svelte';
+	import WordsPullUp from './words/WordsPullUp.svelte';
+</script>
+
+<WordsPullUp words="Hi, I'm" class="text-8xl" />
+<WordsFadeIn words="Martin" class="text-8xl" />
