15f1ff7dafc87f44b6300834912298367a2787b8
Martin Bruun <martinbruun1995@gmail.com>
2024-09-20 19:08:54 +0200

    refactor: moved file location

--- src/components/Hero.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import WordsFadeIn from './words/WordsFadeIn.svelte';
-	import WordsPullUp from './words/WordsPullUp.svelte';
-</script>
-
-<WordsPullUp words="Hi, I'm" class="text-8xl" />
-<WordsFadeIn words="Martin" class="text-8xl" />
