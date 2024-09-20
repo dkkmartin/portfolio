0d00b88238e2d6734dd41a278c434e1d9880e547
Martin Bruun <martinbruun1995@gmail.com>
2024-09-20 19:10:40 +0200

    feat: header

---
+++ src/lib/components/header/Header.svelte
@@ -0,0 +1,15 @@
+<header class="flex justify-between items-center container mx-auto py-4 max-h-[72px]">
+	<div>
+		<h1 class="text-4xl text-primary">MB</h1>
+	</div>
+	<nav class="text-2xl font-thin">
+		<ul class="flex gap-10">
+			<li class="uppercase">
+				<a href="#section-2">Projects</a>
+			</li>
+			<li class="uppercase">
+				<a href="#section-3">Contact</a>
+			</li>
+		</ul>
+	</nav>
+</header>
