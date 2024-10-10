068f00af2545d554e354ce681b5dbaa37945a0cc
Martin Bruun <martinbruun1995@gmail.com>
2024-10-10 20:16:20 +0200

    fix: test fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import { SUPABASE_URL, SUPABASE_KEY } from '$env/static/private'
+import { env } from '$env/dynamic/private'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY)
