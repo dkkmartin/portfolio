a16aef4607049658f7e9618b7149e13abaf57732
Martin Bruun <martinbruun1995@gmail.com>
2024-10-10 23:33:30 +0200

    style: sticky header

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -1,14 +1,16 @@
-<header class="flex justify-between items-center container mx-auto py-4 max-h-[72px]">
+<header
+	class="flex justify-around items-center py-4 max-h-[72px] sticky top-0 z-50 bg-background/80 backdrop-blur-md"
+>
 	<div>
 		<h1 class="text-4xl text-primary">MB</h1>
 	</div>
 	<nav class="text-2xl font-thin">
 		<ul class="flex gap-10">
 			<li class="uppercase">
-				<a href="#section-2">Projects</a>
+				<a href="#section-2">About</a>
 			</li>
 			<li class="uppercase">
-				<a href="#section-3">Contact</a>
+				<a href="#section-3">Projects</a>
 			</li>
 		</ul>
 	</nav>
