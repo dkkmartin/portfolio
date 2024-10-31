e87959f811087bce4714ff558dbd82c8a8247be6
Martin Bruun <martinbruun1995@gmail.com>
2024-10-31 22:30:05 +0100

    refactor: remove logs & update favicon

--- src/lib/stores/activeSection.ts
+++ src/lib/stores/activeSection.ts
@@ -1,5 +1,3 @@
 import { writable } from 'svelte/store'
 
 export const activeSection = writable<string | null>(null)
-
-console.log(activeSection.subscribe((value) => console.log(value)))
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,25 +17,19 @@
 <SectionObserver sectionId="section-2">
 	<section
 		id="section-2"
-		class="min-h-screen flex gap-4 flex-col md:flex-row items-center justify-around bg-secondaryBackground/20 px-4 py-12"
+		class="min-h-[calc(100dvh-50dvh)] flex flex-col justify-center items-center gap-4 bg-secondaryBackground/20 px-4 py-12"
 	>
-		<div class="flex flex-col items-center gap-4">
-			<h2 class="text-4xl font-bold">$~: whoami</h2>
-			<p class="max-w-2xl text-center">
-				I'm a recently graduated front-end developer with a passion for creating beautiful and
-				functional web applications. While I'm new to the professional world of development, I've
-				honed my skills through numerous personal projects, consistently striving for pixel-perfect
-				designs and seamless user experiences. I thrive on problem-solving and enjoy tackling
-				complex challenges to find elegant solutions. My keen attention to detail and commitment to
-				clean, efficient code make me a dedicated and thorough developer. When I'm not coding, you
-				can find me exploring the latest web technologies and frameworks to stay at the cutting edge
-				of front-end development.
-			</p>
-		</div>
-
-		<LayersCard>
-			<img src="https://placehold.co/400x600" alt="Placeholder" />
-		</LayersCard>
+		<h2 class="text-4xl font-bold">About me</h2>
+		<p class="max-w-2xl text-center">
+			I'm a recently graduated front-end developer with a passion for creating beautiful and
+			functional web applications. While I'm new to the professional world of development, I've
+			honed my skills through numerous personal projects, consistently striving for pixel-perfect
+			designs and seamless user experiences. I thrive on problem-solving and enjoy tackling complex
+			challenges to find elegant solutions. My keen attention to detail and commitment to clean,
+			efficient code make me a dedicated and thorough developer. When I'm not coding, you can find
+			me exploring the latest web technologies and frameworks to stay at the cutting edge of
+			front-end development.
+		</p>
 	</section>
 </SectionObserver>
 
