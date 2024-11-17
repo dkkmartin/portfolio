f299e78f5a3d2e9d012488d3948645de0d8df472
Martin Bruun <martinbruun1995@gmail.com>
2024-11-17 21:18:51 +0100

    refactor: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <section className="font-[family-name:var(--font-fira-mono)] flex justify-center items-center h-screen">
-      <h1 className="text-6xl">Martin Bruun</h1>
+    <section className="relative ">
+      <div className="flex items-center gap-6">
+        <div className="border-2 border-gray-500 rounded-full overflow-hidden w-16">
+          <Image src={'/me.webp'} height={70} width={70} alt="Martin bruun" />
+        </div>
+        <div className="flex flex-col">
+          <h1 className="text-2xl font-bold tracking-tight">
+            Hi, I&apos;m Martin
+            <span className="ml-1 inline-block origin-bottom-right animate-wave">👋</span>
+          </h1>
+          <h2 className="underline">Front-end developer</h2>
+        </div>
+      </div>
+
+      <div></div>
     </section>
   );
 }
