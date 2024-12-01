80096524844281301a4bd76ea22269a7ae6b8da0
Martin Bruun <martinbruun1995@gmail.com>
2024-12-01 23:48:34 +0100

    refactor: about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,38 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <section className="relative ">
-      <div className="flex items-center gap-6">
-        <div className="border-2 border-gray-500 rounded-full overflow-hidden w-16">
-          <Image src={'/me.webp'} height={70} width={70} alt="Martin bruun" />
+    <>
+      <section className="relative ">
+        <div className="flex items-center gap-6">
+          <div className="border-2 border-gray-500 rounded-full overflow-hidden w-16">
+            <Image src={'/me.webp'} height={70} width={70} alt="Martin bruun" />
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-2xl font-bold tracking-tight">
+              Hi, I&apos;m Martin
+              <span className="ml-1 inline-block origin-bottom-right animate-wave">👋</span>
+            </h1>
+            <UnderLineAnimation />
+          </div>
         </div>
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-bold tracking-tight">
-            Hi, I&apos;m Martin
-            <span className="ml-1 inline-block origin-bottom-right animate-wave">👋</span>
-          </h1>
-          <UnderLineAnimation />
-        </div>
-      </div>
-      <TechStack />
-    </section>
+      </section>
+      <section className="flex flex-col gap-4 py-4">
+        <p>
+          I&apos;m a front-end developer with a focus on creating functional and interactive user
+          interfaces. I enjoy solving problems and continuously learning new skills to improve my
+          craft.
+        </p>
+
+        <p>
+          In my free time, I love learning and creating fun stuff, such as experimenting with new
+          web technologies, and building small projects. I also enjoy reading tech blogs, watching
+          ThePrimeReagen, and exploring new design trends.
+        </p>
+      </section>
+      <section className="flex flex-col gap-4">
+        <p>current tech stack:</p>
+        <TechStack />
+      </section>
+    </>
   );
 }
