53dab9089f7e947fe511027001ad738ef1a0c085
Martin Bruun <martinbruun1995@gmail.com>
2024-12-03 23:20:50 +0100

    refactor: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
-
 import './globals.css';
+import 'maplibre-gl/dist/maplibre-gl.css';
 import Header from '@/components/header';
 import { Background } from '@/components/background';
 import { ThemeProvider } from 'next-themes';
+import Footer from '@/components/footer';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Martin Bruun',
@@ -18,16 +20,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${GeistSans.className} antialiased `}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+      <Script
+        defer
+        data-website-id="2e00be0e-4962-4f16-be60-73438a291cf8"
+        src="https://umami.martinbruun.dk/script.js"
+      />
+      <body className={`${GeistSans.className} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <Background />
           <Header />
-          <main className="max-w-[576px] mx-auto py-10">{children}</main>
+          <main className="max-w-[576px] mx-auto pt-10 px-6">{children}</main>
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
