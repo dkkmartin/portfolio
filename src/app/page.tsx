54148a0a695ded746e702956fd5c3a780d5a0972
Martin Bruun <martinbruun1995@gmail.com>
2024-12-03 23:21:38 +0100

    refactor: motion sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import MotionSection from '@/components/animation/motionSection';
+import Map from '@/components/map';
+import RecentProjects from '@/components/recentProjects';
 import TechStack from '@/components/techStack';
 import UnderLineAnimation from '@/components/underlineAnimation';
 import Image from 'next/image';
@@ -5,7 +8,10 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <>
-      <section className="relative ">
+      <section className="relative">
+        <Map />
+      </section>
+      <MotionSection className="relative">
         <div className="flex items-center gap-6">
           <div className="border-2 border-gray-500 rounded-full overflow-hidden w-16">
             <Image src={'/me.webp'} height={70} width={70} alt="Martin bruun" />
@@ -18,8 +24,8 @@ export default function Home() {
             <UnderLineAnimation />
           </div>
         </div>
-      </section>
-      <section className="flex flex-col gap-4 py-4">
+      </MotionSection>
+      <MotionSection className="flex flex-col gap-4 py-4 relative">
         <p>
           I&apos;m a front-end developer with a focus on creating functional and interactive user
           interfaces. I enjoy solving problems and continuously learning new skills to improve my
@@ -29,13 +35,16 @@ export default function Home() {
         <p>
           In my free time, I love learning and creating fun stuff, such as experimenting with new
           web technologies, and building small projects. I also enjoy reading tech blogs, watching
-          ThePrimeReagen, and exploring new design trends.
+          tech videos, and exploring new design trends.
         </p>
-      </section>
-      <section className="flex flex-col gap-4">
+      </MotionSection>
+      <MotionSection className="flex flex-col gap-4 relative">
         <p>current tech stack:</p>
         <TechStack />
-      </section>
+      </MotionSection>
+      <MotionSection className="relative">
+        <RecentProjects />
+      </MotionSection>
     </>
   );
 }
